92a839320e18588bb46c5f564767fa5495d46b8f
Henry Till <henrytill@gmail.com>
2024-03-02 22:18:05 -0800

    p99.pl: tweak

--- p99.pl
+++ p99.pl
@@ -1,10 +1,12 @@
-%% P-99: Ninety-Nine Prolog Problems
-%% https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
+%! P-99: Ninety-Nine Prolog Problems
+%  @see https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
 
 %% P01: Find the last element of a list.
 my_last(X,[Y])   :- X = Y.
 my_last(X,[_|Y]) :- my_last(X,Y).
 
-%% Local Variables:
-%% mode: prolog
-%% End:
+%?- my_last(X,[1,2,3,4]).
+%@ X = 4 .
+
+%?- my_last(X,[a,b,c,d]).
+%@ X = d .
