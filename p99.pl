01bdbcab27d2e7519e73b6b6cef8a92c6fd94a01
Henry Till <henrytill@gmail.com>
2024-12-19 18:27:34 -0800

    p99: improve my_last

--- p99.pl
+++ p99.pl
@@ -2,11 +2,11 @@
 %  @see https://www.ic.unicamp.br/~meidanis/courses/mc336/2009s2/prolog/problemas/
 
 %% P01: Find the last element of a list.
-my_last(X,[Y])   :- X = Y.
-my_last(X,[_|Y]) :- my_last(X,Y).
+my_last(X, [X]).
+my_last(X, [_|Y]) :- my_last(X, Y).
 
-%?- my_last(X,[1,2,3,4]).
+%?- my_last(X, [1,2,3,4]).
 %@ X = 4 .
 
-%?- my_last(X,[a,b,c,d]).
+%?- my_last(X, [a,b,c,d]).
 %@ X = d .
