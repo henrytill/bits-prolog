9a1c9c1fb7c50e6b55b607318f5a322000fd4359
Henry Till <henrytill@gmail.com>
2025-01-16 17:46:07 -0800

    p99_test: don't name individual tests

--- p99_test.pl
+++ p99_test.pl
@@ -14,46 +14,26 @@ run_tests(Tests) :-
     ; halt(1)
     ).
 
-test_my_last_basic :-
-    test("my_last: basic",
-         my_last(d, [a,b,c,d])).
-
-test_my_last_empty :-
-    test("my_last: empty",
-         \+ my_last(_, [])).
-
-test_penultimate_basic :-
-    test("penultimate: basic",
-         penultimate(3, [1,2,3,4])).
-
-test_penultimate_empty :-
-    test("penultimate: empty list",
-         \+ penultimate(_, [])).
-
-test_penultimate_singleton :-
-    test("penultimate: singleton list",
-         \+ penultimate(_, [a])).
-
-test_element_at_basic_lookup :-
-    test("element_at: basic lookup",
-         element_at(b, [a,b,c], 2)).
-
-test_element_at_empty :-
-    test("element_at: empty list",
-         \+ element_at(_, [], 1)).
-
-test_element_at_bounds :-
-    test("element_at: out of bounds",
-         \+ element_at(_, [a,b], 3)).
+tests([ test("my_last: basic",
+             my_last(d, [a,b,c,d])),
+        test("my_last: empty",
+             \+ my_last(_, [])),
+        test("penultimate: basic",
+             penultimate(3, [1,2,3,4])),
+        test("penultimate: empty list",
+             \+ penultimate(_, [])),
+        test("penultimate: singleton list",
+             \+ penultimate(_, [a])),
+        test("element_at: basic lookup",
+             element_at(b, [a,b,c], 2)),
+        test("element_at: empty list",
+             \+ element_at(_, [], 1)),
+        test("element_at: out of bounds",
+             \+ element_at(_, [a,b], 3))
+      ]).
 
 main :-
-    run_tests([ test_my_last_basic,
-                test_my_last_empty,
-                test_penultimate_basic,
-                test_penultimate_empty,
-                test_penultimate_singleton,
-                test_element_at_basic_lookup,
-                test_element_at_empty,
-                test_element_at_bounds ]).
+    tests(T),
+    run_tests(T).
 
 :- initialization(main).
