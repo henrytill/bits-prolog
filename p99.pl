587daaa011b72ce44f0822eb3cfeaa09bcd1e667
Henry Till <henrytill@gmail.com>
2025-04-03 20:12:28 -0700

    p99: remove ediprolog comments

--- p99.pl
+++ p99.pl
@@ -7,30 +7,9 @@
 my_last(X, [X]).
 my_last(X, [_|Y]) :- my_last(X, Y).
 
-%?- my_last(X, [1,2,3,4]).
-%@ X = 4 .
-
-%?- my_last(X, [a,b,c,d]).
-%@ X = d .
-
 %% P02: Find the last but one element of a list.
 penultimate(X, [X,_]).
 penultimate(X, [_|Y]) :- penultimate(X, Y).
-
-%?- penultimate(X, [1,2,3,4]).
-%@ X = 3 .
-
-%?- penultimate(X, []).
-%@ false.
-
-%?- penultimate(X, [a]).
-%@ false.
-
-%?- penultimate(X, [a,b]).
-%@ X = a .
-
-%?- penultimate(X, [a,b,c]).
-%@ X = b .
 
 %% P03: Find the K'th element of a list.
 element_at(X, [X|_], 1).
@@ -39,32 +18,8 @@ element_at(X, [_|Y], K) :-
     J is K - 1,
     element_at(X, Y, J).
 
-%?- element_at(X, [a,b,c,d,e], 3).
-%@ X = c .
-
-%?- element_at(X, [a,b,c,d,e], 1).
-%@ X = a .
-
-%?- element_at(X, [a,b,c,d,e], 5).
-%@ X = e .
-
-%?- element_at(X, [a,b,c,d,e], 6).
-%@ false.
-
-%?- element_at(X, [a,b,c,d,e], 0).
-%@ false.
-
 %% P04: Find the number of elements of a list.
 my_length(0, []).
 my_length(Count, [_|Tail]) :-
     my_length(TailCount, Tail),
     Count is TailCount + 1.
-
-%?- my_length(X, []).
-%@ X = 0.
-
-%?- my_length(X, [1]).
-%@ X = 1.
-
-%?- my_length(X, [0,1,2,3,4,5]).
-%@ X = 6.
